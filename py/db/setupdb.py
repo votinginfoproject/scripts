d9efb3a3cd7620b39ca0f4c8be9841202ff8bcac
Jared <jared@votinginfoproject.org>
2010-10-13 12:02:58 -0400

    Merging the master branch with the merged colorado branch. Adding SQLite functionality to the feed creation process.

---
+++ py/db/setupdb.py
@@ -0,0 +1,400 @@
+import sys
+import datetime
+import os.path
+import glob
+import sqlite3
+from datastore import Datastore
+import xml.etree.cElementTree as ET
+from csv import DictReader, QUOTE_ALL, Error as CSVError
+from datetime import datetime, tzinfo
+from utils import get_files
+from xml.sax.saxutils import escape,unescape
+
+def setupdb(filepath):
+  print "Creating database..."
+  datastore = Datastore(filepath)
+  cursor = datastore.connect()
+  
+  cleanup(cursor)
+  datastore.commit()
+  
+  create_tables(cursor)
+  datastore.commit()
+  
+  load_data(cursor)
+  datastore.commit()
+  
+  update_data(cursor)
+  datastore.commit()
+  
+  datastore.close()
+  
+def cleanup(cursor):
+  cursor.execute("DROP TABLE IF EXISTS Early_Vote_Site")
+  cursor.execute("DROP TABLE IF EXISTS Polling_Location")
+  cursor.execute("DROP TABLE IF EXISTS Precinct")
+  cursor.execute("DROP TABLE IF EXISTS Precinct_Polling")
+  cursor.execute("DROP TABLE IF EXISTS Precinct_Early_Vote")
+  cursor.execute("DROP TABLE IF EXISTS Precinct_Split")
+  cursor.execute("DROP TABLE IF EXISTS Electoral_District")
+  cursor.execute("DROP TABLE IF EXISTS Precinct_Split_District")
+  cursor.execute("DROP TABLE IF EXISTS Street_Segment")
+  cursor.execute("DROP TABLE IF EXISTS Election_Administration")
+  cursor.execute("DROP TABLE IF EXISTS Election_Official")
+  cursor.execute("DROP TABLE IF EXISTS Locality")
+  cursor.execute("DROP TABLE IF EXISTS State")
+  cursor.execute("DROP TABLE IF EXISTS Election")
+  cursor.execute("DROP TABLE IF EXISTS VIP_Info")
+  
+def create_tables(cursor):
+  cursor.execute("""CREATE TABLE IF NOT EXISTS Early_Vote_Site
+(
+id INTEGER PRIMARY KEY,
+name TEXT NOT NULL,
+address TEXT NOT NULL,
+city TEXT,
+state TEXT,
+zip TEXT,
+voter_services TEXT
+)""")
+
+  cursor.execute("""CREATE TABLE IF NOT EXISTS Polling_Location
+(
+id INTEGER PRIMARY KEY,
+location_name TEXT,
+line1 TEXT,
+city TEXT,
+state TEXT,
+zip TEXT
+)""")
+
+  cursor.execute("""CREATE TABLE IF NOT EXISTS Precinct_Polling
+(
+polling_location_id INTEGER REFERENCES Polling_Location (id),
+precinct_id INTEGER REFERENCES Precinct (id),
+PRIMARY KEY(polling_location_id,precinct_id)
+)""")
+
+  cursor.execute("""CREATE TABLE IF NOT EXISTS Precinct
+(
+id INTEGER PRIMARY KEY,
+name TEXT NOT NULL,
+locality_id INTEGER NOT NULL,
+mail_only TEXT DEFAULT 'No' NOT NULL
+)""")
+
+  cursor.execute("""CREATE TABLE IF NOT EXISTS Precinct_Split
+(
+id INTEGER PRIMARY KEY,
+name TEXT NOT NULL,
+precinct_id INTEGER NOT NULL REFERENCES Precinct (id)
+)""")
+
+  cursor.execute("""CREATE TABLE IF NOT EXISTS Electoral_District
+(
+id INTEGER PRIMARY KEY,
+name TEXT,
+type TEXT,
+number INTEGER
+)""")
+
+  cursor.execute("""CREATE TABLE IF NOT EXISTS Precinct_Split_District
+(
+precinct_split_id INTEGER NOT NULL REFERENCES Precinct_Split (id),
+electoral_district_id INTEGER NOT NULL REFERENCES Electoral_District (id),
+PRIMARY KEY(precinct_split_id, electoral_district_id)
+)""")
+
+  cursor.execute("""CREATE TABLE IF NOT EXISTS Street_Segment
+(
+id CHAR PRIMARY KEY,
+start_house_number INTEGER NOT NULL,
+end_house_number INTEGER NOT NULL,
+odd_even_both TEXT,
+start_apartment_number INTEGER,
+end_apartment_number INTEGER,
+street_direction TEXT,
+street_name TEXT NOT NULL,
+street_suffix TEXT NOT NULL,
+address_direction TEXT,
+state TEXT NOT NULL,
+city TEXT NOT NULL,
+zip TEXT NOT NULL,
+precinct_id INTEGER REFERENCES Precinct (id),
+precinct_split_id INTEGER REFERENCES Precint_Split (id)
+)""")
+
+  cursor.execute("""CREATE TABLE IF NOT EXISTS Election_Administration
+(
+id INTEGER PRIMARY KEY,
+name TEXT,
+eo_id INTEGER,
+mailing_address TEXT,
+city TEXT,
+state TEXT,
+zip TEXT,
+zip_plus TEXT,
+elections_url TEXT,
+registration_url TEXT,
+am_i_registered_url TEXT,
+absentee_url TEXT,
+where_do_i_vote_url TEXT
+)""")
+
+  cursor.execute("""CREATE TABLE IF NOT EXISTS Election_Official
+(
+id INTEGER PRIMARY KEY,
+name TEXT,
+title TEXT,
+phone TEXT,
+fax TEXT,
+email TEXT
+)""")
+
+  cursor.execute("""CREATE TABLE IF NOT EXISTS Locality
+(
+id INTEGER PRIMARY KEY,
+name TEXT,
+state_id TEXT NOT NULL,
+type TEXT,
+election_administration_id INTEGER REFERENCES Election_Administration (id)
+)""")
+
+  cursor.execute("""CREATE TABLE IF NOT EXISTS State
+(
+id INTEGER PRIMARY KEY,
+name TEXT,
+election_administration_id INTEGER REFERENCES Election_Administration (id),
+organization_url TEXT
+)""")
+
+  cursor.execute("""CREATE TABLE IF NOT EXISTS Precinct_Early_Vote
+(
+precinct_id INTEGER REFERENCES Precinct (id),
+early_vote_site_id INTEGER REFERENCES Early_Vote_Site (id),
+PRIMARY KEY(precinct_id, early_vote_site_id)
+)""")
+
+  cursor.execute("""CREATE TABLE IF NOT EXISTS Election
+(
+id INTEGER PRIMARY KEY,
+date TEXT,
+election_type TEXT,
+state_id INTEGER REFERENCES State (id),
+statewide TEXT,
+registration_info TEXT
+)""")
+
+  cursor.execute("""CREATE TABLE IF NOT EXISTS VIP_Info
+(
+id INTEGER PRIMARY KEY,
+source_id INTEGER,
+description TEXT,
+state_id INTEGER REFERENCES State (id)
+);""")
+
+def load_data(cursor):
+  file_tmpl = '/Users/jared/Documents/projects/vip/data/ms/{0}.txt'
+  
+  possible_data = (
+    'source',
+    'election_administration',
+    'state',
+    'election',
+    'election_official',
+    'locality',
+    'polling_location',
+    'precinct',
+    'precinct_split',
+    'street_segment',
+  )
+  
+  for i in possible_data:
+    if os.path.exists(file_tmpl.format(i)):
+      with open(file_tmpl.format(i),'r') as r:
+        print "Parsing and loading data from {0}".format(i)
+        reader = DictReader(r, delimiter='\t')
+        
+        try:
+          for line in reader:
+            if i=='source':
+              cursor.execute(
+                "INSERT INTO VIP_Info(id,source_id,description,state_id) VALUES (?,?,?,?)",
+                (
+                  line['VIP_ID'],
+                  line['ID'],
+                  line['DESCRIPTION'],
+                  28,
+                )
+              )
+              
+            elif i=='election_administration':
+              cursor.execute(
+                "INSERT OR IGNORE INTO Election_Administration(id,name,eo_id,mailing_address,city,state,zip,zip_plus,elections_url,registration_url,am_i_registered_url,absentee_url,where_do_i_vote_url) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)",
+                (
+                  line['ID'],
+                  line.get('NAME', None),
+                  line.get('EO_ID', None),
+                  line.get('MAILING_ADDRESS', None),
+                  line.get('CITY', None),
+                  line.get('STATE', None),
+                  line.get('ZIP', None),
+                  line.get('ZIP_PLUS', None),
+                  line.get('ELECTIONS_URL', None),
+                  line.get('REGISTRATION_URL', None),
+                  line.get('AM_I_REGISTERED_URL', None),
+                  line.get('ABSENTEE_URL', None),
+                  line.get('WHERE_DO_I_VOTE_URL', None),
+                )
+              )
+            
+            elif i=='state':
+              cursor.execute(
+                "INSERT INTO State(id,name,election_administration_id,organization_url) VALUES (?,?,?,?)",
+                (
+                  line['ID'],
+                  line.get('NAME'),
+                  line.get('ELECTION_ADMINISTRATION_ID'),
+                  'http://www.sos.ms.gov/elections.aspx',
+                )
+              )
+            
+            elif i=='election':
+              cursor.execute(
+                "INSERT OR IGNORE INTO Election(id,date,election_type,state_id,statewide,registration_info) VALUES (?,?,?,?,?,?)",
+                (
+                  line['ID'],
+                  datetime.strptime(line.get('DATE'), '%m/%d/%Y').strftime('%Y-%m-%d'),
+                  line.get('ELECTION_TYPE', "General"),
+                  line.get('STATE_ID', 28),
+                  line.get('STATEWIDE', "Yes"),
+                  line.get('REGISTRATION_INFO', None),
+                )
+              )
+            
+            elif i=='election_official':
+              cursor.execute(
+                "INSERT INTO Election_Official(id,name,title,phone,fax,email) VALUES (?,?,?,?,?,?)",
+                (
+                  line['ID'],
+                  line.get('NAME'),
+                  line.get('TITLE', None),
+                  line.get('PHONE', None),
+                  line.get('FAX', None),
+                  line.get('EMAIL', None)
+                )
+              )
+              
+            elif i=='locality':
+              cursor.execute(
+                "INSERT OR IGNORE INTO Locality(id,name,state_id,type,election_administration_id) VALUES (?,?,?,?,?)",
+                (
+                  line['ID'],
+                  line.get('NAME'),
+                  line.get('STATE_ID', 28),
+                  line.get('TYPE'),
+                  line.get('ELECTION_ADMINISTRATION_ID'),
+                )
+              )
+            
+            elif i=='polling_location':
+              cursor.execute(
+                "INSERT OR IGNORE INTO Polling_Location(id,location_name,line1,city,state,zip) VALUES (?,?,?,?,?,?)",
+                (
+                  line['ID'],
+                  line.get('LOCATION_NAME'),
+                  line.get('LINE1'),
+                  line.get('CITY'),
+                  line.get('STATE'),
+                  line.get('ZIP'),
+                )
+              )
+              
+            elif i=='precinct':
+              cursor.execute(
+                "INSERT OR IGNORE INTO Precinct(id,name,locality_id,mail_only) VALUES (?,?,?,?)",
+                (
+                  line['ID'],
+                  line.get('NAME'),
+                  line.get('LOCALITY_ID'),
+                  line.get('MAIL_ONLY',"No"),
+                )
+              )
+                
+              if len(line.get('POLLING_LOCATION_ID',""))>0:
+                cursor.execute(
+                  "INSERT OR IGNORE INTO Precinct_Polling(precinct_id,polling_location_id) VALUES (?,?)",
+                  (
+                    line['ID'],
+                    line['POLLING_LOCATION_ID'],
+                  )
+                )
+              
+              if len(line.get('EARLY_VOTE_SITE_ID',""))>0:
+                cursor.execute(
+                  "INSERT OR IGNORE INTO Precinct_Early_Vote(precinct_id,early_vote_site_id) VALUES (?,?)",
+                  (
+                    line['ID'],
+                    line['EARLY_VOTE_SITE_ID'],
+                  )
+                )
+            
+            elif i=='precinct_split':
+              if len(line.get('PRECINCT_ID',""))>0:
+                cursor.execute(
+                  "INSERT OR IGNORE INTO Precinct_Split(id,name,precinct_id) VALUES (?,?,?)",
+                  (
+                    line['ID'],
+                    line.get('NAME'),
+                    line.get('PRECINCT_ID'),
+                  )
+                )
+                
+                continue
+              
+              if len(line.get('ELECTORAL_DISTRICT_ID',""))>0:
+                cursor.execute(
+                  "INSERT OR IGNORE INTO Precinct_Split_District(precinct_split_id,electoral_district_id) VALUES (?,?)",
+                  (
+                    line['ID'],
+                    line['ELECTORAL_DISTRICT_ID'],
+                  )
+                )
+            
+            elif i=='street_segment':
+              cursor.execute(
+                "INSERT INTO Street_Segment(id,start_house_number,end_house_number,odd_even_both,start_apartment_number,end_apartment_number,street_direction,street_name,street_suffix,address_direction,state,city,zip,precinct_id,precinct_split_id) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
+                (
+                  line.get('STREET_SEGMENT_ID'),
+                  line.get('START_HOUSE_NUMBER', None),
+                  line.get('END_HOUSE_NUMBER', None),
+                  line.get('ODD_EVEN_BOTH', None),
+                  line.get('START_APARTMENT_NUMBER', None),
+                  line.get('END_APARTMENT_NUMBER', None),
+                  line.get('STREET_DIRECTION', None),
+                  line.get('STREET_NAME', None),
+                  line.get('STREET_SUFFIX', None),
+                  line.get('ADDRESS_DIRECTION', None),
+                  line.get('STATE', None),
+                  line.get('CITY', None),
+                  line.get('ZIP', None),
+                  line.get('PRECINCT_ID', None),
+                  line.get('PRECINCT_SPLIT_ID', None),
+                )
+              )
+              
+        except CSVError, e:
+          sys.exit("file {0}, line {1}: {2}".format(file_tmpl.format(i), reader.line_num, e))
+        
+        except sqlite3.IntegrityError, e:
+          sys.exit("file {0}, line {1}: {2}".format(file_tmpl.format(i), reader.line_num, e))
+
+def update_data(cursor):
+  print "Updating street segments..."
+  cursor.execute("UPDATE Street_Segment SET start_house_number=1, end_house_number=9999999 WHERE start_house_number=0 AND end_house_number=0")
+  
+  print "Updating locality election official data..."
+  cursor.execute("UPDATE Locality SET election_administration_id=NULL WHERE election_administration_id NOT IN (SELECT id FROM Election_Administration)")
+  
+  print "Updating Precinct Polling data..."
+  cursor.execute("DELETE FROM Precinct_Polling WHERE polling_location_id NOT IN (SELECT id FROM Polling_Location)")
---
+++ py/vip_feed_sqlite.py
@@ -0,0 +1,375 @@
+import sys
+import datetime
+import os.path
+import glob
+import sqlite3
+import xml.etree.cElementTree as ET
+from db.datastore import Datastore
+from db.setupdb import setupdb
+from csv import DictReader,Error as CSVError
+from datetime import datetime, tzinfo
+from utils import get_files
+from xml.sax.saxutils import escape,unescape
+
+def main():
+  now = datetime.now()
+  vip_file = "{0}vipFeed-{1}.xml".format('/Users/jared/Documents/projects/vip/data/ms/','28')
+  
+  setupdb('/tmp/vip_data_co.db')
+  
+  with open(vip_file,'w') as w:
+    w.write('<?xml version="1.0" standalone="yes"?>\n<vip_object xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="http://election-info-standard.googlecode.com/files/vip_spec_v2.2.xsd" schemaVersion="2.2">\n')
+    
+    create_header(w,now)
+    create_election(w)
+    create_election_admins(w)
+    create_election_officials(w)
+    create_localities(w)
+    create_precincts(w)
+    create_precinct_splits(w)
+    create_street_segments(w)
+    create_polling_locations(w)
+    
+    w.write('</vip_object>')
+
+def create_header(w,now):
+  print "Creating source and state elements..."
+  datastore = Datastore('/tmp/vip_data_co.db')
+  cursor = datastore.connect()
+  cursor.execute("SELECT VIP_Info.source_id AS source_id, State.name AS state_name, VIP_Info.id AS vip_id, ? AS datetime, description, organization_url, election_administration_id FROM VIP_Info, State WHERE State.id=VIP_Info.state_id",(now.strftime('%Y-%m-%dT%H:%M:%S'),))
+
+  row = cursor.fetchone()
+
+  w.write("""  <source id="{source_id}">
+    <name>State of {state_name}</name>
+    <vip_id>{vip_id}</vip_id>
+    <datetime>{datetime}</datetime>
+    <description>{description}</description>
+    <organization_url>{organization_url}</organization_url>
+  </source>
+  <state id="{vip_id}">
+    <name>{state_name}</name>
+    <election_administration_id>{election_administration_id}</election_administration_id>
+  </state>
+""".format(**row))
+
+def create_election(w):
+  print "Creating election elements..."
+  datastore = Datastore('/tmp/vip_data_co.db')
+  cursor = datastore.connect()
+  
+  cursor.execute("SELECT * FROM Election")
+  
+  for row in cursor:
+    root = ET.Element("election",id=unicode(row['id']))
+    
+    date = ET.SubElement(root,"date")
+    date.text = row['date']
+    
+    election_type = ET.SubElement(root,"election_type")
+    election_type.text = row['election_type']
+    
+    state_id = ET.SubElement(root,"state_id")
+    state_id.text = unicode(row['state_id'])
+    
+    statewide = ET.SubElement(root,"statewide")
+    statewide.text = row['statewide']
+    
+    registration_info = ET.SubElement(root,"registration_info")
+    registration_info.text = unicode(row['registration_info'])
+    
+    w.write(ET.tostring(root))
+    
+  datastore.close()
+  
+def create_election_admins(w):
+  """Writes all election administration information"""
+  print "Creating election administration elements..."
+  datastore = Datastore('/tmp/vip_data_co.db')
+  cursor = datastore.connect()
+  
+  cursor.execute("SELECT * FROM Election_Administration")
+
+  for row in cursor:
+    root = ET.Element("election_administration",id=unicode(row['id']))
+    
+    name = ET.SubElement(root,"name")
+    name.text = row['name']
+    
+    if row['eo_id'] is not None:
+      eo_id = ET.SubElement(root,"eo_id")
+      eo_id.text = unicode(row['eo_id'])
+    
+    if row['mailing_address'] is not None and len(row['mailing_address'])>0:
+      mailing = ET.SubElement(root,"mailing_address")
+      line1 = ET.SubElement(mailing,"line1")
+      line1.text = escape(row['mailing_address'])
+      city = ET.SubElement(mailing,"city")
+      city.text = row['city']
+      state = ET.SubElement(mailing,"state")
+      state.text = row['state']
+      zipcode = ET.SubElement(mailing,"zip")
+      
+      if row['zip_plus'] is not None and len(row['zip_plus'])>0:
+        zipcode.text = "{0}-{1}".format(row['zip'],row['zip_plus'])
+      else:
+        zipcode.text = row['zip']
+      
+    if row['elections_url'] is not None and len(row['elections_url'])>0:
+      elections_url = ET.SubElement(root,"elections_url")
+      elections_url.text = row['elections_url']
+      
+    if row['registration_url'] is not None and len(row['registration_url'])>0:
+      registration_url = ET.SubElement(root,"registration_url")
+      registration_url.text = row['registration_url']
+    
+    if row['am_i_registered_url'] is not None and len(row['am_i_registered_url'])>0:
+      am_i_registered_url = ET.SubElement(root,"am_i_registered_url")
+      am_i_registered_url.text = row['am_i_registered_url']
+    
+    if row['absentee_url'] is not None and len(row['absentee_url'])>0:
+      absentee_url = ET.SubElement(root,"absentee_url")
+      absentee_url.text = row['absentee_url']
+    
+    if row['where_do_i_vote_url'] is not None and len(row['where_do_i_vote_url'])>0:
+      where_do_i_vote = ET.SubElement(root,"where_do_i_vote_url")
+      where_do_i_vote.text = row['where_do_i_vote_url']
+    
+    w.write(ET.tostring(root))
+  
+  datastore.close()
+
+def create_election_officials(w):
+  """Writes all election official information"""
+  print "Creating election official elements..."
+  datastore = Datastore('/tmp/vip_data_co.db')
+  cursor = datastore.connect()
+  
+  cursor.execute("SELECT * FROM Election_Official")
+
+  for row in cursor:
+    root = ET.Element("election_official",id=unicode(row['id']))
+    
+    name = ET.SubElement(root,"name")
+    name.text = row['name']
+    
+    if row['title'] is not None:
+      title = ET.SubElement(root,"title")
+      title.text = unicode(row['title'])
+    
+    if row['phone'] is not None:
+      phone = ET.SubElement(root,"phone")
+      phone.text = row['phone']
+      
+    if row['fax'] is not None:
+      fax = ET.SubElement(root,"fax")
+      fax.text = row['fax']
+    
+    if row['email'] is not None:
+      email = ET.SubElement(root,"email")
+      email.text = unicode(row['email'])
+      
+    w.write(ET.tostring(root))
+  
+  datastore.close()
+  
+def create_localities(w):
+  print "Creating locality elements..."
+  datastore = Datastore('/tmp/vip_data_co.db')
+  cursor = datastore.connect()
+  
+  cursor.execute("SELECT * FROM Locality")
+  
+  for row in cursor:
+    root = ET.Element("locality",id=unicode(row['id']))
+    
+    name = ET.SubElement(root,"name")
+    name.text = row['name']
+    
+    state_id = ET.SubElement(root,"state_id")
+    state_id.text = unicode(row['state_id'])
+    
+    locality_type = ET.SubElement(root,"type")
+    locality_type.text = row['type']
+    
+    if row['election_administration_id'] is not None:
+      election_administration_id = ET.SubElement(root,"election_administration_id")
+      election_administration_id.text = unicode(row['election_administration_id'])
+    
+    w.write(ET.tostring(root))
+    
+  datastore.close()
+  
+def create_precincts(w):
+  """Writes all election administration information"""
+  print "Creating precinct elements..."
+  datastore = Datastore('/tmp/vip_data_co.db')
+  cursor = datastore.connect()
+  
+  mem = Datastore(':memory:')
+  c = mem.connect()
+  c.execute("""CREATE TABLE IF NOT EXISTS Precinct_Polling
+(
+polling_location_id INTEGER,
+precinct_id INTEGER,
+PRIMARY KEY(polling_location_id, precinct_id)
+)""")
+
+  cursor.execute("SELECT * FROM Precinct_Polling")
+  
+  for row in cursor:
+    c.execute("INSERT INTO Precinct_Polling VALUES (?,?)",(list(row)))
+  
+  mem.commit()
+  
+  cursor.execute("SELECT * FROM Precinct WHERE locality_id IS NOT NULL AND name IS NOT NULL")
+  
+  for row in cursor:    
+    root = ET.Element("precinct",id=unicode(row['id']))
+    name = ET.SubElement(root,"name")
+    name.text = row['name']
+    
+    locality_id = ET.SubElement(root,"locality_id")
+    locality_id.text = unicode(row['locality_id'])
+    
+    if row['mail_only'] is not None and len(row['mail_only'])>0:
+      mail_only = ET.SubElement(root,"mail_only")
+      mail_only.text = row['mail_only']
+      
+      if row['mail_only']=='Yes':
+        w.write(ET.tostring(root))
+        continue
+      else:
+        c.execute("SELECT polling_location_id FROM Precinct_Polling WHERE precinct_id=?",(row['id'],))
+
+        for p in c:
+          polling_location_id = ET.SubElement(root,"polling_location_id")
+          polling_location_id.text = unicode(p['polling_location_id'])
+    
+    w.write(ET.tostring(root))
+  
+  datastore.close()
+
+def create_precinct_splits(w):
+  print "Creating precinct split elements..."
+  datastore = Datastore('/tmp/vip_data_co.db')
+  cursor = datastore.connect()
+  
+  cursor.execute("SELECT * FROM Precinct_Split")
+  
+  for row in cursor:
+    root = ET.Element("precinct_split",id=unicode(row['id']))
+    
+    name = ET.SubElement(root,"name")
+    name.text = row['name']
+    
+    precinct_id = ET.SubElement(root,"precinct_id")
+    precinct_id.text = unicode(row['precinct_id'])
+    
+    w.write(ET.tostring(root))
+    
+  datastore.close()
+
+def create_street_segments(w):
+  print "Creating street segment elements..."
+  datastore = Datastore('/tmp/vip_data_co.db')
+  cursor = datastore.connect()
+  
+  cursor.execute("SELECT * FROM Street_Segment WHERE street_name IS NOT NULL")
+  
+  for row in cursor:
+    root = ET.Element("street_segment",id=unicode(row['id']))
+    
+    if len(unicode(row['start_house_number']))>0 and len(unicode(row['end_house_number']))>0:
+      if row['start_house_number']>row['end_house_number']:
+        start = unicode(row['end_house_number'])
+        end = unicode(row['start_house_number'])
+      else:
+        start = unicode(row['start_house_number'])
+        end = unicode(row['end_house_number'])
+      
+      start_house_number = ET.SubElement(root,"start_house_number")
+      start_house_number.text = start
+      
+      end_house_number = ET.SubElement(root,"end_house_number")  
+      end_house_number.text = end
+    
+    if len(row['odd_even_both'])>0:
+      odd_even_both = ET.SubElement(root,"odd_even_both")
+      odd_even_both.text = unicode(row['odd_even_both'])
+    
+    if row['start_apartment_number'] is not None and row['end_apartment_number'] is not None and len(unicode(row['start_apartment_number']))>0 and len(unicode(row['end_apartment_number']))>0:
+      start_apartment_number = ET.SubElement(root,"start_apartment_number")
+      start_apartment_number.text = unicode(row['start_apartment_number'])
+      
+      end_apartment_number = ET.SubElement(root,"end_apartment_number")
+      end_apartment_number.text = unicode(row['end_apartment_number'])
+    
+    non_house_address = ET.SubElement(root,"non_house_address")
+    
+    if len(row['street_direction'])>0:
+      street_direction = ET.SubElement(non_house_address,'street_direction')
+      street_direction.text = row['street_direction']
+    
+    street_name = ET.SubElement(non_house_address,"street_name")
+    street_name.text = row['street_name']
+    
+    if len(row['street_suffix'])>0:
+      street_suffix = ET.SubElement(non_house_address,"street_suffix")
+      street_suffix.text = row['street_suffix']
+    
+    if row['address_direction'] is not None and len(row['address_direction'])>0:
+      address_direction = ET.SubElement(non_house_address,"address_direction")
+      address_direction.text = row['address_direction']
+    
+    city = ET.SubElement(non_house_address,"city")
+    city.text = row['city']
+    
+    state = ET.SubElement(non_house_address,"state")
+    state.text = row['state']
+    
+    zip = ET.SubElement(non_house_address,"zip")
+    zip.text = row['zip']
+
+    if row['precinct_id'] is not None:
+      precinct_id = ET.SubElement(root,"precinct_id")
+      precinct_id.text = unicode(row['precinct_id'])
+      
+    if row['precinct_split_id'] is not None:
+      precinct_split_id = ET.SubElement(root,"precinct_split_id")
+      precinct_split_id.text = unicode(row['precinct_split_id'])
+
+    w.write(ET.tostring(root))
+  
+  datastore.close()
+  
+def create_polling_locations(w):
+  print "Creating polling place elements..."
+  datastore = Datastore('/tmp/vip_data_co.db')
+  cursor = datastore.connect()
+  
+  cursor.execute("SELECT * FROM Polling_Location WHERE line1 IS NOT NULL")
+  
+  for row in cursor:
+    root = ET.Element("polling_location",id=unicode(row['id']))
+    
+    address = ET.SubElement(root,"address")
+    
+    if row['location_name'] is not None:
+      location_name = ET.SubElement(address,"location_name")
+      location_name.text = escape(row['location_name'])
+    
+    line1 = ET.SubElement(address,"line1")
+    line1.text = escape(row['line1'])
+    city = ET.SubElement(address,"city")
+    city.text = row['city']
+    state = ET.SubElement(address,"state")
+    state.text = row['state']
+    zipcode = ET.SubElement(address,"zip")
+    
+    w.write(ET.tostring(root))
+  
+  datastore.close()
+  
+if __name__ == "__main__":
+  main()
