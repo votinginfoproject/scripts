97998561d092b99bbe32cec6b75658a129a6046e
Jared <jared@votinginfoproject.org>
2011-07-18 12:23:06 -0400

    Added code to strip unneeded whitespace and escape data values. Also, for consistency, all fieldnames converted to uppercase.

--- py/db/setupdb.py
+++ py/db/setupdb.py
@@ -302,6 +302,8 @@ def load_data(cursor, config):
               quotechar=config.get(sect, 'quotechar'),
               quoting=QUOTE_MINIMAL
             )
+            
+            reader.fieldnames = map(str.upper, reader.fieldnames)
           elif parser_type=='regex':
             reader = re.compile(config.get(sect,'regex'))
             #reader = DictReader(r, delimiter=chr(config.getint(i.title(),'delimiter')), quotechar='"', quoting=QUOTE_MINIMAL)
@@ -315,6 +317,14 @@ def load_data(cursor, config):
         
         try:
           for line in get_line(r, reader, parser_type):
+            # trim off any whitespace and escape the values
+            try:
+              line.update(zip(line.iterkeys(), map(str.strip, line.itervalues())))
+              line.update(zip(line.iterkeys(), map(escape, line.itervalues())))
+            except:
+              # if this fails, print the offending line
+              print line
+
             if i=='source':
               cursor.execute(
                 "INSERT INTO VIP_Info(id,source_id,description,state_id) VALUES (?,?,?,?)",
@@ -476,44 +486,48 @@ def load_data(cursor, config):
               
               if line.get('STREET_DIRECTION','')=='NULL':
                 line['STREET_DIRECTION'] = None
-                
-              cursor.execute(
-                """INSERT INTO
-                  Street_Segment(
-                  id,
-                  start_house_number,
-                  end_house_number,
-                  odd_even_both,
-                  start_apartment_number,
-                  end_apartment_number,
-                  street_direction,
-                  street_name,
-                  street_suffix,
-                  address_direction,
-                  state,
-                  city,
-                  zip,
-                  precinct_id,
-                  precinct_split_id
-                ) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)""",
-                (
-                  line.get('ID'),
-                  line.get('START_HOUSE_NUMBER', None),
-                  line.get('END_HOUSE_NUMBER', None),
-                  line.get('ODD_EVEN_BOTH', None),
-                  line.get('START_APARTMENT_NUMBER', None),
-                  line.get('END_APARTMENT_NUMBER', None),
-                  line.get('STREET_DIRECTION', None),
-                  line.get('STREET_NAME', None),
-                  line.get('STREET_SUFFIX', None),
-                  line.get('ADDRESS_DIRECTION', None),
-                  line.get('STATE', config.get('Main', 'state_abbreviation')),
-                  line.get('CITY', None),
-                  line.get('ZIP', None),
-                  line.get('PRECINCT_ID', None),
-                  line.get('PRECINCT_SPLIT_ID', None),
+              
+              try:  
+                cursor.execute(
+                  """INSERT INTO
+                    Street_Segment(
+                    id,
+                    start_house_number,
+                    end_house_number,
+                    odd_even_both,
+                    start_apartment_number,
+                    end_apartment_number,
+                    street_direction,
+                    street_name,
+                    street_suffix,
+                    address_direction,
+                    state,
+                    city,
+                    zip,
+                    precinct_id,
+                    precinct_split_id
+                  ) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)""",
+                  (
+                    line.get('ID'),
+                    line.get('START_HOUSE_NUMBER', None),
+                    line.get('END_HOUSE_NUMBER', None),
+                    line.get('ODD_EVEN_BOTH', None),
+                    line.get('START_APARTMENT_NUMBER', None),
+                    line.get('END_APARTMENT_NUMBER', None),
+                    line.get('STREET_DIRECTION', None),
+                    line.get('STREET_NAME', None),
+                    line.get('STREET_SUFFIX', None),
+                    line.get('ADDRESS_DIRECTION', None),
+                    line.get('STATE', config.get('Main', 'state_abbreviation')),
+                    line.get('CITY', None),
+                    line.get('ZIP', None),
+                    line.get('PRECINCT_ID', None),
+                    line.get('PRECINCT_SPLIT_ID', None),
+                  )
                 )
-              )
+                
+              except:
+                print line
               
         except CSVError, e:
           sys.exit("file {0}, line {1}: {2}".format(filename, reader.line_num, e))
